38462ae3af1750e94aaeb2a61035209377866b95
Alexander <zlajsobaka@gmail.com>
2023-12-04 13:15:26 +0300

    TODO: check time creation pof-file

--- main.py
+++ main.py
@@ -7,117 +7,186 @@
 # Script GoST Compilation.py v.1.0      #
 #                                       #
 #########################################################################
-import glob
 import os
-import sys
+import subprocess
 from datetime import datetime
-
-# max_qpf: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_NEW.dpf
-# max_dir: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\
-# max_qsf: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_NEW.qsf
-# max_pof: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_NEW.pof
-# max_const: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\
-# max_ML: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\C_ML_TEST.tdf
-# max_ST: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\C_ST_TEST.tdf
-# max_XL0: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X0L.v
-# max_XS0: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X0S.v
-# max_XL1: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X1L.v
-# max_XS1: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X1S.v
-# max_XL2: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X2L.v
-# max_XS2: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X2S.v
-# max_XL3: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X3L.v
-# max_XS3: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X3S.v
-# max_XL4: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X4L.v
-# max_XS4: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X4S.v
-# max_XL5: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X5L.v
-# max_XS5: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X5S.v
-# max_XL6: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X6L.v
-# max_XS6: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X6S.v
-# max_XL7: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X7L.v
-# max_XS7: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X7S.v
-# cyc_qpf: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW.qpf
-# cyc_dir: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\
-# cyc_qsf: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW.qsf
-# cyc_pof: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW.pof
-# cyc_const: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\
-# cyc_mif: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\ROM_FLASH_CYCLON.mif
-# cyc_c24: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\CON_24bit.tdf
-# cyc_XL0: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X0L.v
-# cyc_XS0: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X0S.v
-# cyc_XL1: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X1L.v
-# cyc_XS1: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X1S.v
-# cyc_XL2: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X2L.v
-# cyc_XS2: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X2S.v
-# cyc_XL3: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X3L.v
-# cyc_XS3: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X3S.v
-# cyc_XL4: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X4L.v
-# cyc_XS4: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X4S.v
-# cyc_XL5: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X5L.v
-# cyc_XS5: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X5S.v
-# cyc_XL6: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X6L.v
-# cyc_XS6: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X6S.v
-# cyc_XL7: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X7L.v
-# cyc_XS7: R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\X7S.v
-# qua_map: C:\Altera\10.1\quartus\bin\quartus_map.exe
-# qua_dir: C:\Altera\10.1\quartus\bin\
-# fil_fol: R:\Niir\GostCrypt\
+import secrets
+import shutil
+from tqdm import tqdm
 
 
-class Max:
+def const_edit(name, new_value):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), name), 'r') as _f:
+        _data = _f.read()
+    return _data.replace('6666', new_value)
 
-    def __init__(self, start_folder):
+
+def mif1_edit(name, new_keys):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), name), 'r') as _f:
+        _data = _f.read()
+    for i in range(0, 8):
+        _data = _data.replace(f'66{i}', new_keys[i])
+    return _data
+
+
+def mif2_edit(name, new_value):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), name), 'r') as _f:
+        _data = _f.read()
+    _data = _data.replace('6660', new_value[:2])
+    _data = _data.replace('6661', new_value[2:4])
+    _data = _data.replace('6662', new_value[4:])
+    return _data
+
+
+def qua_cmd(qua_type, qpf, qsf, set_files=None):
+    _exe = os.path.join(r'c:\altera\13.0sp1\quartus\bin', f'quartus_{qua_type}.exe')
+    try:
+        if set_files is None:
+            process = subprocess.run([_exe, qpf, '-c', qsf], check=True, capture_output=True)
+        else:
+            process = subprocess.run([_exe, f'--read_settings_files={set_files}', '--write_settings_files=off', qpf,
+                                      '-c', qsf], check=True, capture_output=True)
+    except subprocess.CalledProcessError as e:
+        tqdm.write(f"Ошибка команды {e.cmd}!")
+    else:
+        if 'was successful. 0 errors' in str(process.stdout):
+            return True
+        else:
+            return False
+
+
+class Fpga:
+
+    def __init__(self, start_folder, _keys, _module, _type, _lib):
+        self.keys = _keys
+        self.key32 = secrets.token_hex(8)
+        self.cam = _module
+        self.fpga = _type
+        self.lib_dir = _lib
+        self.cam_file = os.path.normpath(fr"R:\Niir\GostCrypt\Modules\{self.cam}_{self.fpga}.pof")
+        self.error_ini = False
         for root, dirs, files in os.walk(start_folder):
             for file in files:
                 if file.endswith('.qpf'):
-                    self.max_qpf = os.path.join(root, file)
+                    self.qpf = os.path.join(root, file)
                 if file.endswith('.qsf'):
-                    self.max_qsf = os.path.join(root, file)
+                    self.qsf = os.path.join(root, file)
                 if file.endswith('.pof'):
-                    self.max_pof = os.path.join(root, file)
-        self.const = os.path.join(start_folder, 'GOST_MAX_LIB')
-        for root, dirs, files in os.walk(self.const):
-            for file in files:
-                if file == 'C_ML_TEST.tdf':
-                    self.max_ML = os.path.join(root, file)
-                if file == 'C_ST_TEST.tdf':
-                    self.max_ST = os.path.join(root, file)
-        # max_const: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\
-        # max_ML: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\C_ML_TEST.tdf
-        # max_ST: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\C_ST_TEST.tdf
-        # max_XL0: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X0L.v
-        # max_XS0: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X0S.v
-        # max_XL1: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X1L.v
-        # max_XS1: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X1S.v
-        # max_XL2: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X2L.v
-        # max_XS2: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X2S.v
-        # max_XL3: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X3L.v
-        # max_XS3: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X3S.v
-        # max_XL4: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X4L.v
-        # max_XS4: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X4S.v
-        # max_XL5: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X5L.v
-        # max_XS5: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X5S.v
-        # max_XL6: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X6L.v
-        # max_XS6: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X6S.v
-        # max_XL7: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X7L.v
-        # max_XS7: R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2\GOST_MAX_LIB\X7S.v
+                    self.pof = os.path.join(root, file)
+                    self.pof_date = os.path.getctime(self.pof)
+        self.const = os.path.join(start_folder, self.lib_dir)
+        if self.fpga == 'max':
+            if os.path.isfile(os.path.join(self.const, 'C_ML_TEST.tdf')):
+                self.ML = os.path.join(self.const, 'C_ML_TEST.tdf')
+            else:
+                self.error_ini = True
+            if os.path.isfile(os.path.join(self.const, 'C_ST_TEST.tdf')):
+                self.ST = os.path.join(self.const, 'C_ST_TEST.tdf')
+            else:
+                self.error_ini = True
+        else:
+            if os.path.isfile(os.path.join(self.const, 'ROM_FLASH_CYCLON.mif')):
+                self.rom_flash = os.path.join(self.const, 'ROM_FLASH_CYCLON.mif')
+            else:
+                self.error_ini = True
+            if os.path.isfile(os.path.join(self.const, 'ud92g.mif')):
+                self.ud92g = os.path.join(self.const, 'ud92g.mif')
+            else:
+                self.error_ini = True
+            if os.path.isfile(os.path.join(self.const, 'CON_24bit.tdf')):
+                self.con_24bit = os.path.join(self.const, 'CON_24bit.tdf')
+            else:
+                self.error_ini = True
+        self.x_ls = []
+        for i in range(0, 8):
+            self.x_ls.append({'L': ''})
+            if os.path.isfile(os.path.join(self.const, f'X{i}L.v')):
+                self.x_ls[i]['L'] = os.path.join(self.const, f'X{i}L.v')
+            else:
+                self.error_ini = True
+            self.x_ls[i]['S'] = ''
+            if os.path.isfile(os.path.join(self.const, f'X{i}S.v')):
+                self.x_ls[i]['S'] = os.path.join(self.const, f'X{i}S.v')
+            else:
+                self.error_ini = True
 
     def run(self):
-        pass
-# // формирование имени конечного файла //
-# // выбор ключей
-# // замена ключей для MAX
-# // ввод 32-разрядных констант
-# // ввод ключей для MAX
+        if not self.error_ini and not os.path.isfile(self.cam_file):
+            self.edit_files()
+            if self.compile():
+                tqdm.write(f'{self.fpga} compilation OK')
+                # if os.path.getctime(self.pof) > self.pof_date:
+                shutil.copyfile(self.pof, self.cam_file)
+                return True
+                # else:
+                #     return False
+            else:
+                tqdm.write(f'{self.fpga} compilation ERROR - need recompile!')
+                return False
 
-# // замена ключей для CYCLON
-# // замена номера модуля в константе CON_24bit.tdf
-# замена номера модуля в R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3\GOST_NEW_LIB\ud92g.mif
-# Замена адресов с 073 по 076. В адресе 073 ввести 00. В адресах 074, 75, 076 ввести соответствующие три байта модуля.
-# // ввод ключей для CYCLON в tdf
-# // ввод ключей для CYCLON в ROM_FLASH_CYCLON.mif
+    def edit_files(self):
+        tqdm.write(f'Замена констант для {self.fpga}')
+        if self.fpga == 'max':
+            with open(self.ML, 'w') as _f:
+                _f.write(const_edit(name=fr"{self.lib_dir}\{os.path.basename(self.ML)}",
+                                    new_value=str(int(self.key32[:8], 16))))
+            with open(self.ST, 'w') as _f:
+                _f.write(const_edit(name=fr"{self.lib_dir}\{os.path.basename(self.ST)}",
+                                    new_value=str(int(self.key32[8:], 16))))
+        else:
+            with open(self.con_24bit, 'w') as _f:
+                _f.write(const_edit(name=fr"{self.lib_dir}\{os.path.basename(self.con_24bit)}",
+                                    new_value=str(self.cam)))
+            with open(self.rom_flash, 'w') as _f:
+                _f.write(mif1_edit(name=fr"{self.lib_dir}\{os.path.basename(self.rom_flash)}", new_keys=self.keys))
+            with open(self.ud92g, 'w') as _f:
+                _f.write(mif2_edit(name=fr"{self.lib_dir}\{os.path.basename(self.ud92g)}", new_value=f'{self.cam:06X}'))
+        for i in range(0, 8):
+            with open(self.x_ls[i]['L'], 'w') as _f:
+                _f.write(const_edit(name=fr"{self.lib_dir}\{os.path.basename(self.x_ls[i]['L'])}",
+                                    new_value=str(int(self.keys[i][4:], 16))))
+            with open(self.x_ls[i]['S'], 'w') as _f:
+                _f.write(const_edit(name=fr"{self.lib_dir}\{os.path.basename(self.x_ls[i]['S'])}",
+                                    new_value=str(int(self.keys[i][:4], 16))))
 
-# COMPILATION
+    def compile(self):
+        tqdm.write('Начинается компиляция проекта')
+        compile_result = qua_cmd('map', self.qpf, self.qsf, set_files='on')
+        if compile_result:
+            tqdm.write('qua_map OK')
+            compile_result = qua_cmd('fit', self.qpf, self.qsf, set_files='off')
+        else:
+            return False
+        if compile_result:
+            tqdm.write('qua_fit OK')
+            if self.fpga == 'max':
+                compile_result = qua_cmd('asm', self.qpf, self.qsf, set_files='on')
+            else:
+                compile_result = qua_cmd('asm', self.qpf, self.qsf, set_files='off')
+        else:
+            return False
+        if compile_result:
+            tqdm.write('qua_asm OK')
+            compile_result = qua_cmd('sta', self.qpf, self.qsf)
+        else:
+            return False
+        if compile_result:
+            tqdm.write('qua_sta OK')
+            return True
+        else:
+            return False
 
 
-max_compilation = Max(start_folder=os.path.normpath(r"R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2"))
-max_compilation.run()
+for number in tqdm(range(14330, 14355), ncols=80, ascii=True):
+    tqdm.write(str(datetime.now()))
+    tqdm.write(f'-=CAM #{number}=-')
+    error_compilation = False
+    while not error_compilation:
+        keys = [secrets.token_hex(4) for i in range(0, 8)]
+        max_project = Fpga(start_folder=os.path.normpath(r"R:\Niir\GostCrypt\Qua_projects\MAX_28042018_restored_2"),
+                           _keys=keys, _module=number, _type='max', _lib='GOST_MAX_LIB')
+        if max_project.run():
+            cyc = Fpga(start_folder=os.path.normpath(r"R:\Niir\GostCrypt\Qua_projects\CYCL_28042018_restored3"),
+                       _keys=keys, _module=number, _type='cyc', _lib='GOST_NEW_LIB')
+            error_compilation = cyc.run()
+    tqdm.write('==================')
